52d858d99b7a81a6aec6e2815c31cc6025086604
Arvindkumar7742 <arvindkumar14032004@gmail.com>
2024-05-21 10:42:06 +0530

    ci:

---
+++ src/MyButton.js
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+
+// function Square() {
+//     return <button className="square">X</button>;
+//   }
+// function Mybutton(props) {
+   
+//     return (
+//         <span onClick={clickHandler} >
+//             {value}
+//         </span>
+//     )
+// }
+// export default Mybutton;
