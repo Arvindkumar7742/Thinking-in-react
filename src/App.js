8027de5d41fa38aae7bbb104bec5db7cf7377d22
Arvindkumar7742 <arvindkumar14032004@gmail.com>
2024-05-21 12:33:48 +0530

    CI:Project completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,93 @@
 import { useState } from "react";
 import "./App.css"
+import fruitData from './data.js'
 
-function App() {
+function ProductRow({ product }) {
+  const name = product.stocked ? product.name :
+    <span style={{ color: 'red' }}>
+      {product.name}
+    </span>;
+
+return (
+  <tr>
+    <td>{name}</td>
+    <td>{product.price}</td>
+  </tr>
+);
+  
+}
+
+function ProductCategoryRow({ category }){
+  return(
+    <tr>
+      <th colSpan="2">
+        {category}
+      </th>
+    </tr>
+  )
+}
+
+function ProductTable({ products }){
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category} />
+      );
+    }
+    rows.push(
+      <ProductRow
+        product={product}
+        key={product.name} />
+    );
+    lastCategory = product.category;
+  });
+
   return (
-    <div className="App">
-    
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+function App() {
+  const [newData,changeNewData] = useState(fruitData);
+  function changeHandler1(target){
+    if(target.target.checked){
+      let filteredData = newData.filter((data)=>{
+        if(data.stocked==true){
+          return data;
+        }
+      })
+      console.log(filteredData);
+      changeNewData(filteredData);
+    }
+    else{
+      changeNewData(fruitData);
+    }
+  }
+  function changeHandler2(target){
+    //Pending will do in future
+  }
+  return (
+    <div>
+      <div>
+        <input type="text" id="fname" name="fname" placeholder="Search..." onChange={changeHandler2}/><br/>
+        <input type="checkbox" id="check" name="checkName" onChange={changeHandler1}/>
+        <label for="checkName" >{' '}Check the stocked products</label>
+      </div>
+    <div className="ProductTable">
+      <ProductTable  products={newData}/>
+    </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,13 @@
+let fruitData =[
+    { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+    { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+    { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+    { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+    { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+    { category: "Vegetables", price: "$10", stocked: true, name: "tamatar" },
+    { category: "Vegetables", price: "$12", stocked: true, name: "bangan" },
+    { category: "Vegetables", price: "$20", stocked: false, name: "moongfali" },
+    { category: "Vegetables", price: "$30", stocked: false, name: "sitfal" },
+]
+export default fruitData;
